61d7928e2f5fa18be1d82bd95f2769fad50f4502
SteffieClat <stefaniegroendal@outlook.dk>
2025-11-24 13:50:41 +0100

    Update StartMenuScript.cs

--- Assets/Core/UI/UIScripts/StartMenuScript.cs
+++ Assets/Core/UI/UIScripts/StartMenuScript.cs
@@ -105,8 +105,6 @@ public class StartMenuScript : MonoBehaviour
         if (sceneName != "Quiz_Stefanie")
             yield return SceneManager.LoadSceneAsync(timerScene, LoadSceneMode.Additive);
 
-        yield return SceneManager.LoadSceneAsync(timerScene, LoadSceneMode.Additive);
-
         yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
 
         yield return SceneManager.UnloadSceneAsync(gameObject.scene);
